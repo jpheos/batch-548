ca6663338922f0054601cbca799d5a6c3a255b2d
Joseph BLANCHARD <jooo.blanchard@gmail.com>
2021-01-26 11:43:28 +0100

    add 01-Programming-basics/Lecture

---
+++ 01-Ruby/01-Programming-basics/Lecture/methods.rb
@@ -0,0 +1,41 @@
+require 'date'
+
+# def hello(name)
+#   return "Hello #{name}"
+# end
+
+
+# puts hello('John')
+# puts hello('Paul')
+# puts hello('Ringo')
+
+
+# def full_name(first_name, last_name)
+#   "#{first_name.capitalize} #{last_name.upcase}"
+# end
+
+# puts full_name("boris", "paillard")   # => "Boris Paillard"
+# puts full_name("seb", "saunier")
+
+
+# def max(x, y)
+#   if x > y
+#     return x
+#   else
+#     return y
+#   end
+# end
+
+# first_number = 2
+# second_number = 1
+# largest_number = max(first_number, second_number)
+# puts largest_number   # => 5
+
+
+# def tomorrow
+#   tomorrow_date = Date.today + 1
+#   return tomorrow_date.strftime("%B %d")
+# end
+
+
+# puts tomorrow
---
+++ 01-Ruby/01-Programming-basics/Lecture/variables.rb
@@ -0,0 +1,7 @@
+age = 17
+
+puts "age: #{age}"
+
+age = age + 1
+
+puts "age: #{age}"
