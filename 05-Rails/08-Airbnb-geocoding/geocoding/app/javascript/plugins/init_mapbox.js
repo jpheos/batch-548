a0d7b6e4cef461bd3f91a1395e05d7f7fee831f3
thibault173 <thibault.villard@gmail.com>
2021-03-10 10:18:17 +0100

    Add geocoding

---
+++ 05-Rails/08-Airbnb-geocoding/geocoding/app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,53 @@
+import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) { // only build a map if there's a div#map to inject into
+    // Search API KEy
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+
+    // Create a map
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb'
+    });
+
+    // Search on map
+    map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+                                      mapboxgl: mapboxgl }));
+
+    // add markers
+    const markers = JSON.parse(mapElement.dataset.markers);
+    markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.infoWindow);
+
+      // Create a HTML element for your custom marker
+      const element = document.createElement('div');
+      element.className = 'marker';
+      element.style.backgroundImage = `url('${marker.image_url}')`;
+      element.style.backgroundSize = 'contain';
+      element.style.width = '25px';
+      element.style.height = '25px';
+
+      new mapboxgl.Marker(element)
+        .setLngLat([ marker.lng, marker.lat ])
+        .setPopup(popup)
+        .addTo(map);
+    });
+
+    // Define function
+    const fitMapToMarkers = (map, markers) => {
+      const bounds = new mapboxgl.LngLatBounds();
+      markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
+      map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+    };
+
+    // Call function
+    fitMapToMarkers(map, markers);
+  }
+};
+
+export { initMapbox };
